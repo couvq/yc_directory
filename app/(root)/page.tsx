cdfe30f085c23caca2be5808c876d65e8aaa9f92
couvq <quentincouvelaire@gmail.com>
2024-12-04 19:49:48 -0500

    finished with search

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,22 @@
-import Image from "next/image";
+import SearchForm from "../components/SearchForm";
 
-const Home = () => {
+type HomePageProps = {
+  searchParams: Promise<{ query?: string }>;
+};
+
+const Home = async ({ searchParams }: HomePageProps) => {
+  const query = (await searchParams).query;
   return (
     <>
       <section className="pink_container">
         <h1 className="heading">
           Pitch your startup, <br /> Connect with entrepreneurs
         </h1>
-        <p className="sub-heading !max-w-3xl">Submit ideas, vote on pitches, and get noticed in virtual competitions.</p>
+        <p className="sub-heading !max-w-3xl">
+          Submit ideas, vote on pitches, and get noticed in virtual
+          competitions.
+        </p>
+        <SearchForm query={query} />
       </section>
     </>
   );
